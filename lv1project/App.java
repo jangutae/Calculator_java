d38708921a347dbc7989704c2928a54c8a05df61
jangutae <jsilll101389@gmail.com>
2024-10-16 21:42:31 +0900

    App.java

--- lv1project/App.java
+++ lv1project/App.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 양의 정수(0 포함)를 입려받기
+        
 
         boolean flag = true;
         while (flag) {
+            
             System.out.print("첫 번째 숫자를 입력하세요 : ");
             int number1 = sc.nextInt();
+          
             System.out.print("두 번째 숫자를 입력하세요 : ");
             int number2 = sc.nextInt();
             double result = 0;
-            // 사직연산 기호 ('-' , '+', '*', '/')
+           
 
             System.out.print("사칙연산 기호를 입력하세요 : ");
             char operator = sc.next().charAt(0);
-
-            // 양의 정수 2개와 사칙연산 기호를 사용하여 연산 진행 후 결과값 츨력
+ 
             switch (operator) {
                 case '+':
                     double result1 = number1 + number2;
